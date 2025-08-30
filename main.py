3514a255934b8575e6fd36a68886254b0a4122c8
thakyislam <sadiataki11@gmail.com>
2025-08-30 21:15:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+import _tkinter
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+# Keyboard controls
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    try:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+
+    except _tkinter.TclError:
+
+        game_is_on = False
+
+
+try:
+    screen.mainloop()
+except _tkinter.TclError:
+    pass
