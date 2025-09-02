d54b61f8efb395bb9f1dbeed4357ce2c7d92b572
thakyislam <sadiataki11@gmail.com>
2025-09-02 15:03:53 +0200

    final version

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
-import _tkinter
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,8 +12,8 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
-# Keyboard controls
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,21 +22,31 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    try:
-        screen.update()
-        time.sleep(0.1)
-        snake.move()
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-        if snake.head.distance(food) < 15:
-            food.refresh()
-            snake.extend()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-    except _tkinter.TclError:
-
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
-try:
-    screen.mainloop()
-except _tkinter.TclError:
-    pass
+
+
+
+screen.exitonclick()
